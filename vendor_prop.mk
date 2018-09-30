706dba64c3d44bd9ec5afadd874cba43ac88f3fd
SagarMakhar <sagarmakhar@gmail.com>
2018-09-30 08:27:57 +0000

    X00TD: disable rescue party

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -173,6 +173,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.telephony.default_network=20,20 \
 	telephony.lteOnCdmaDevice=1
 
+# Rescue Party
+PRODUCT_PROPERTY_OVERRIDES += \
+        persist.sys.disable_rescue=true
+
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.rmnet.data.enable=true\
